38a2559ea45db861ad796614559d8dbca0255d84
theikeofficial <ajzenhamernikola@gmail.com>
2019-12-27 20:27:13 +0100

    Small change for `bundle install`

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "INPUT_DESTINATION_DIR: ${INPUT_DESTINATION_DIR:=_site}"
 echo "INPUT_ADD_NOJEKYLL_TAG: ${ADD_NOJEKYLL_TAG:=true}"
 
 gem install bundler:2.0.2
-bundle install
+bundle install --full-index
 bundle exec jekyll build --verbose --destination "${INPUT_DESTINATION_DIR}"
 
 if [ $ADD_NOJEKYLL_TAG = true ]
